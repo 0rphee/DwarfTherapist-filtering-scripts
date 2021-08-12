e3306c430c4c4f0df4ce2952f39c0ac8cda1742b
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2021-08-11 21:43:03 -0500

    Add files via upload

---
+++ Python Scripts/Dwarves<5.py
@@ -0,0 +1,15 @@
+stringy = ""
+
+for n in range(84):
+    stringy += (f"((d.labor_enabled({n}) && d.labor_rating({n}) < 5) || d.labor_enabled({n}) == false) &&")
+
+print(stringy)
+
+"""
+sec_str = ""
+
+for n in range(84):
+    sec_str +=(f"(d.labor_enabled({n}) && d.labor_rating({n}) > 8) ||")
+
+print(sec_str)
+"""
